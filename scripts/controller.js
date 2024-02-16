9889c5d3e9da4a9346a69dbe415f11881a57eb2e
irakli <iraklitsurtsumia@gmail.com>
2024-02-16 15:00:28 +0400

    stuck on view

--- scripts/controller.js
+++ scripts/controller.js
@@ -1,13 +1,14 @@
 //import { quizz } from "./model.js";
 //console.log(quizz);
+import { as } from "./view.js";
 import { loadQuizz } from "./model.js";
-const gu = async function () {
+export const controlQuizzData = async function () {
   try {
     const huh = await loadQuizz();
     console.log(huh);
   } catch {}
 };
-gu();
+controlQuizzData();
 
 /* const doUKnow = async function () {
   try {
---
+++ scripts/view.js
@@ -0,0 +1,17 @@
+import { controlQuizzData } from "./controller.js";
+
+export default class QuestionsView {
+  _parentElement = document.querySelector(".question--area");
+}
+
+`
+<article>
+<h2>question here</h2>
+<p>option</p>
+<p>option</p>
+<p>option</p>
+<p>option</p>
+<button>prev</button>
+<button>next</button>
+</article>
+`;
