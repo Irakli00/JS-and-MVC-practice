40d1ec87661e9052876e7239252859bd5d5c1b9c
irakli <iraklitsurtsumia@gmail.com>
2024-02-19 15:02:49 +0400

    made data go to view, but gotta set it to this.data

--- scripts/controller.js
+++ scripts/controller.js
@@ -1,9 +1,12 @@
 import * as model from "./model.js";
+import questionsView from "./view.js";
 
 const controlQuizzData = async function () {
   try {
     await model.loadQuizz();
     console.log(model.state.data);
+
+    questionsView.render(model.state.data[0]);
   } catch {}
 };
 controlQuizzData();
--- scripts/view.js
+++ scripts/view.js
@@ -1,17 +1,26 @@
-import { controlQuizzData } from "./controller.js";
-
-export default class QuestionsView {
+class QuestionsView {
   _parentElement = document.querySelector(".question--area");
+  _data;
+
+  render(data) {
+    /*     this.data = data;
+    console.log(this._data);
+    console.log(data); */
+    const markup = `
+    <article>
+      <h2>${data.question}</h2>
+      <p>${data.incorrect_answers[0]}</p>
+      <p>${data.incorrect_answers[1]}</p>
+      <p>${data.incorrect_answers[2]}</p>
+      <p>${data.correct_answer}</p>
+      <button>prev</button>
+      <button>next</button>
+    </article>
+    `;
+    this._parentElement.insertAdjacentHTML("afterbegin", markup);
+    //console.log(markup);
+    //console.log(this._data);
+  }
 }
 
-`
-<article>
-<h2>question here</h2>
-<p>option</p>
-<p>option</p>
-<p>option</p>
-<p>option</p>
-<button>prev</button>
-<button>next</button>
-</article>
-`;
+export default new QuestionsView();
