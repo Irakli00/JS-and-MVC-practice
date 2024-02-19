992766f425f8033c7a9d28732317675ff7fa3f3a
irakli <iraklitsurtsumia@gmail.com>
2024-02-19 16:45:06 +0400

    randomised answers

--- scripts/view.js
+++ scripts/view.js
@@ -6,21 +6,45 @@ class QuestionsView {
     /*     this.data = data;
     console.log(this._data);
     console.log(data); */
+    const allAnswers = [data.correct_answer, ...data.incorrect_answers];
+    const randomisedAnswers = [];
+    console.log(allAnswers);
+
+    const randomise = function (arr) {
+      arr.slice().forEach(() => {
+        let index = Math.floor(Math.random() * arr.length);
+        randomisedAnswers.push(arr[index]);
+        arr.splice(arr.indexOf(arr[index]), 1);
+      });
+    };
+
+    randomise(allAnswers);
     const markup = `
-    <article>
-      <h2>${data.question}</h2>
-      <p>${data.incorrect_answers[0]}</p>
-      <p>${data.incorrect_answers[1]}</p>
-      <p>${data.incorrect_answers[2]}</p>
-      <p>${data.correct_answer}</p>
-      <button>prev</button>
-      <button>next</button>
-    </article>
-    `;
+      <article>
+        <h2>${data.question}</h2>
+        <p>${randomisedAnswers[0]}</p>
+        <p>${randomisedAnswers[1]}</p>
+        <p>${randomisedAnswers[2]}</p>
+        <p>${randomisedAnswers[3]}</p>
+        <button>prev</button>
+        <button>next</button>
+      </article>
+      `;
     this._parentElement.insertAdjacentHTML("afterbegin", markup);
+    randomise();
     //console.log(markup);
     //console.log(this._data);
   }
 }
 
 export default new QuestionsView();
+
+/*     const allAnswers = [data.correct_answer, ...data.incorrect_answers];
+    const randomisedAnswers = [];
+    console.log(allAnswers);
+
+    allAnswers.slice().forEach(() => {
+      let index = Math.floor(Math.random() * allAnswers.length);
+      randomisedAnswers.push(allAnswers[index]);
+      allAnswers.splice(allAnswers.indexOf(allAnswers[index]), 1);
+    }); */
